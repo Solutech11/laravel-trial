3c6957960950c5f0541ae65ae8f5c94bec9557f0
Solutech11 <soluwizy@gmail.com>
2024-06-03 01:04:25 -1200

    updated

--- app/Http/Controllers/app/auth/authController.php
+++ app/Http/Controllers/app/auth/authController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\app\auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+use function Laravel\Prompts\error;
 
 class authController extends Controller
 {
@@ -12,8 +15,19 @@ class authController extends Controller
     }
 
     public function loginFunc(Request $request){
-        return $request->input();
+        try{
+
+            $request->validate([
+                'email'=>['required', 'email'],
+                'password'=>['required', 'string', 'max:15', 'min:8'],
+            ]);
+
+            return "goes well";
+        }catch(ValidationException $e){
+            return $e->errors();
+        }
     }
+
     public function registerPAge(){
         return view("app.pages.auth.registerPage");
     }
