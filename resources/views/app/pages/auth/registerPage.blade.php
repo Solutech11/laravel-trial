828c37bd58f076ec26427b3127b38f531f05339b
Solutech11 <soluwizy@gmail.com>
2024-06-06 15:10:22 +0100

    update

--- resources/views/app/pages/auth/registerPage.blade.php
+++ resources/views/app/pages/auth/registerPage.blade.php
@@ -22,7 +22,7 @@
                 <label for="name">Name</label>
                 <input type="text" class="rounded-md p-1 border focus:outline-green-500 " name="name" id="name">
                 @error('name')
-                <div class="text-red-500"><p>{{$message}}</p></div>   
+                    <div><p class="text-red-500"><p>{{$message}}</p></div>   
                 @enderror
             </div>
 
@@ -30,15 +30,16 @@
                 <label for="email">Email</label>
                 <input type="email" class="rounded-md p-1 border focus:outline-green-500 " name="email" id="email">
                 @error('email')
-                <div class="text-red-500"><p>{{$message}}</p></div>   
+                    <div><p class="text-red-500"><p>{{$message}}</p></div>   
                 @enderror
             </div>
 
             <div class="flex flex-col justify-start w-1/2">
                 <label for="password">Password</label>
                 <input type="password"  name="password" class="rounded-md p-1 border focus:outline-green-500" id="password">
+               
                 @error('password')
-                <div class="text-red-500"><p>{{$message}}</p></div>   
+                    <div><p class="text-red-500"><p>{{$message}}</p></div>   
                 @enderror
             </div>
 
