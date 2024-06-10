c529d71b35702167a57df15ddf01ec03d849f66d
Solutech11 <soluwizy@gmail.com>
2024-06-10 13:25:13 +0100

    update

--- database/migrations/2024_06_10_104422_tbl_users.php
+++ database/migrations/2024_06_10_104422_tbl_users.php
@@ -11,22 +11,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        //
+        Schema::create("tbl_user", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->string("email");
+            $table->string("password");
             $table->timestamps();
-        });
+        
+     });
+        
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
+
     }
 };
